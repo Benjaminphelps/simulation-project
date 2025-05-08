35c8b96cccbba49b791801e8dfc3649edef0855a
Drinu07 <github@andreascic.slmail.me>
2025-05-08 13:12:05 +0200

    Modify comment(s) and slight update to arrival_hours.csv column 2 title

--- sim/interarrivals.py
+++ sim/interarrivals.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 
 
-# Open csv file
 # %%
 arrivals_dataset = pd.read_csv('../data/arrival_hours.csv', delimiter=';')
 arrivals_dataset['Share_of_charging_transactions'] = arrivals_dataset['Share_of_charging_transactions'].str.replace(',','.').astype(float)
@@ -11,13 +10,11 @@ arrivals_dataset.head()
 # %%
 
 # %%
-# I'm changing this so you pass it a single hour, as per reccomendation on may 6 meeting.
+# Generate arrivals for a given hour
 def generate_arrivals_per_hour(hour):
     mu = arrivals_dataset['Share_of_charging_transactions'][hour] * 750
     print ("mu: ", mu)
     times = np.sort(np.random.uniform(0,60,np.random.poisson(mu))).tolist()
-    # print(arrivals_ph)
-    # print(sum(arrivals_ph))
     return times
 
 # Generate arrivals for some hour 0-24, will fluctuate based on the current time in simulation
