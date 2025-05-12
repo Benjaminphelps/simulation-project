015ba2c1a971d82581d43d30aaf8f18c125a7cc3
Drinu07 <github@andreascic.slmail.me>
2025-05-12 12:37:19 +0200

    remove csv.head() - only used for initial test

--- sim/rng_models.py
+++ sim/rng_models.py
@@ -6,7 +6,6 @@ import pandas as pd
 def generate_arrivals_per_hour(hour):
     arrivals_dataset = pd.read_csv('../data/arrival_hours.csv', delimiter=';')
     arrivals_dataset['Share_of_charging_transactions'] = arrivals_dataset['Share_of_charging_transactions'].str.replace(',','.').astype(float)
-    arrivals_dataset.head()
 
     mu = arrivals_dataset['Share_of_charging_transactions'][hour] * 750
     times = np.sort(np.random.uniform(hour,hour+1,np.random.poisson(mu))).tolist()
